c5f0afeb13773303ae0257f6dc0de9d87685bf0d
zgulen <gulenzubeyir@gmail.com>
2022-07-06 21:48:08 +0300

    routed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
-import MyNavbar from './components/Nav';
+import Nav from './components/Nav';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Instructors from './pages/Instructors';
 import NotFound from './pages/NotFound';
 
+
 function App() {
   return (
     <>
-      <MyNavbar />
-      <Home />
-      <Instructors />
-      <Contact />
-      <NotFound />
-      <Footer />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/instructors' element={<Instructors />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,14 @@
 import logo from '../img/logo.png';
+import {Link, NavLink} from "react-router-dom"
+
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-md navbar-light">
       <div className="container-fluid">
-        <a href="/" className="navbar-brand">
+        <Link to="/" className="navbar-brand">
           <img src={logo} alt="" />
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -18,18 +20,18 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 me-3 mb-lg-0">
             <li className="nav-item">
-              <a href="/" className="nav-link active" aria-current="page">
+              <NavLink to="/" style={({isActive}) => ({color: isActive && "red"})} className="nav-link" aria-current="page">
                 Home
-              </a>
+              </NavLink>
             </li>
 
-            <a href="/instructors" className="nav-link">
+            <NavLink style={({isActive}) => ({color: isActive && "red"})} to="/instructors" className="nav-link">
               Instructors
-            </a>
+            </NavLink>
             <li className="nav-item">
-              <a href="/contact" className="nav-link">
+              <NavLink style={({isActive}) => ({color: isActive && "red"})} to="/contact" className="nav-link">
                 Contact
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
