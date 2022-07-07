fac7ef1e441ce3ba9ab62f49774f3bf9e6b52745
zgulen <gulenzubeyir@gmail.com>
2022-07-07 21:42:42 +0300

    inclass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Instructors from './pages/Instructors';
 import NotFound from './pages/NotFound';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import InstructorDetail from './pages/InstructorDetail';
+import Path from "./pages/Path"
+import Aws from './pages/Aws';
+import FullStack from './pages/FullStack';
 
 function App() {
   return (
@@ -13,11 +16,16 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/instructors' element={<Instructors />} />
-          <Route path='/instructors/:id' element={<InstructorDetail/>} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/*' element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/instructors" element={<Instructors />} />
+          <Route path="/instructors/:id" element={<InstructorDetail />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path='path' element={<Path />} >
+            <Route index element={<FullStack />} />
+            <Route path='fullstack' index element={<FullStack />} />
+            <Route path='aws' element={<Aws />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
